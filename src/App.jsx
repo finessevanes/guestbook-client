8100af81ebecf1cab3870489960fd09e982285a7
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2023-02-20 18:39:28 -0700

    add disabled button to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Web3Button } from "@web3modal/react";
 import {
-  useAccount,
   useContractRead,
   useContractWrite,
   usePrepareContractWrite,
@@ -12,7 +11,6 @@ import Entry from "./components/Entry";
 
 function App() {
   const [newEntry, setNewEntry] = useState("");
-  const { isConnected } = useAccount();
   const abi = guestBook.abi;
   const contractAddress = "0x61912362D631f0e09e2e0E7934F725097bECc05b";
 
@@ -49,23 +47,17 @@ function App() {
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="w-full max-w-lg">
         <Web3Button />
-        {isConnected && (
-          <>
-            <GuestBookForm
-              handleAddEntry={handleAddEntry}
-              newEntry={newEntry}
-              handleNewEntryChange={handleNewEntryChange}
-            />
-          </>
-        )}
-        {dataEntries.length > 0 ? (
+        <GuestBookForm
+          handleAddEntry={handleAddEntry}
+          newEntry={newEntry}
+          handleNewEntryChange={handleNewEntryChange}
+        />
+        {Boolean(dataEntries.length) && (
           <div className="grid grid-cols-1 gap-4">
-            {dataEntries.map((entry, index) => (
-              <Entry key={index} entry={entry} />
-            )).reverse()}
+            {dataEntries
+              .map((entry, index) => <Entry key={index} entry={entry} />)
+              .reverse()}
           </div>
-        ) : (
-          <h1>Nothing</h1>
         )}
       </div>
     </div>
--- src/components/GuestBookForm.jsx
+++ src/components/GuestBookForm.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { useAccount } from "wagmi";
 
 const GuestBookForm = ({ handleAddEntry, newEntry, handleNewEntryChange }) => {
-  
+  const { isConnected } = useAccount();
   return (
     <div className="bg-white shadow-md rounded px-8 py-6 mb-8">
       <h1 className="text-4xl font-bold mb-4">Guest Book</h1>
@@ -22,8 +23,9 @@ const GuestBookForm = ({ handleAddEntry, newEntry, handleNewEntryChange }) => {
         </div>
         <div className="flex justify-end">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline disabled:opacity-50 disabled:cursor-not-allowed"
             type="submit"
+            disabled={!isConnected}
           >
             Add Entry
           </button>
